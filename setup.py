badf6b57368cd4fd3117d330d93eb9da97a2cb50
kavyamittal1234 <68001757+kavyamittal1234@users.noreply.github.com>
2022-10-05 15:02:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+import plotly.express as px
+import csv
+import numpy as np
+
+def plotFigure(data_path):
+    with open(data_path) as csv_file:
+        df = csv.DictReader(csv_file)
+        fig = px.scatter(df,x="Days Present", y="Marks In Percentage")
+        fig.show()
